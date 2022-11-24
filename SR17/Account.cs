a37f6b754216b68350e226e44a7719c9fd90a9c8
Юлия Волкова <volktul@gmail.com>
2022-11-24 21:28:05 +0300

    Добавьте файлы проекта.

---
+++ SR17/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR17
+{
+    internal class Account<T>
+    {
+        private T Num { get; set; }
+        private string Name { get; set; }
+        private int Balance { get; set; }
+
+        public string GetInfo()
+        {
+            return $"{Num} {Name} {Balance}";
+        }
+        public void Input()
+        {
+            Console.WriteLine("Введите номер счета");
+            Num = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите ФИО");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите баланс");
+            Balance = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ SR17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account<int> account1 = new Account<int>();
+            account1.Input();
+            Console.WriteLine(account1.GetInfo());
+
+            Account<string> account2 = new Account<string>();
+            account2.Input();
+            Console.WriteLine(account2.GetInfo());
+            
+            Console.ReadKey();
+        }
+    }
+}
